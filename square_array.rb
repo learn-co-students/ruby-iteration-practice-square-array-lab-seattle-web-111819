7ec4a777f5f1ad5801e0b2e2f560ca28cc1dde7f
jmknopf1007 <jmknopf1007@gmail.com>
2019-11-20 09:19:20 -0800

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,20 @@
-def square_array(array)
+#numbers = [1,2,3]
+
+def square_array(numbers)
   # your code here
+  new_array = []
+  numbers.each do |v|
+    new_array << (v ** 2)   
+  end
+  new_array
 end
+
+
+# def square_array(array)
+#   # your code here
+#   array.map do |v|
+#     v ** 2
+#   end
+# end
+
+
